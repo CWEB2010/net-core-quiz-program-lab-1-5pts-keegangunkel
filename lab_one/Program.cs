81af341204646efb9243b0f5a43f6ba2d1079df3
Keegan Gunkel <gunkeec@dunwoody.edu>
2020-01-22 14:33:59 -0600

    Added an array with the welcome text printed to screen

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace lab_one
+namespace GitActivityA
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] correctAnswers = new string[] { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
+            Console.WriteLine("Welcome to the .NET Core quiz!\nYou will have 10 questions to answer, all multiple choice\n\n");
+            Console.WriteLine("1.) What is the system support by .NET Core?\nA.)Mac\nB.)Windows\nC.)Linux\nD.)None of the above");
+
         }
     }
 }
